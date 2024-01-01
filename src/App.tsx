d09394c49a2fc4f8a8223416104f16d4f7b331ae
nikunjp-tatva <nikunj.polara@tatvasoft.com>
2024-01-01 18:15:48 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
